1f63771a65599c65efea132ffd70ad0cdc969ee6
Bertrand Dubaut <b.dubaut@gmail.com>
2017-01-11 23:47:54 +0100

    remove comments

--- shell/prompt.sh
+++ shell/prompt.sh
@@ -2,12 +2,6 @@ if [[ -e /usr/lib/git-core/git-sh-prompt ]]; then
   . /usr/lib/git-core/git-sh-prompt
 fi
 
-# function __git_in_repo() { __gitdir > /dev/null; }
-# function __git_is_dirty() { git status --porcelain | grep -q .; }
-# function __git_dirty() { __git_in_repo && __git_is_dirty && echo "$1"; }
-# function __git_clean() { __git_in_repo && (__git_is_dirty || echo "$1"); }
-#
-# export PS1='$(__git_ps1 "\[\e[37m\](\[\e[0m\]")$(__git_dirty "\[\e[35m\]✗\[\e[0m\]")$(__git_clean "\[\e[32m\]✓\[\e[0m\]")$(__git_ps1 " \[\e[34m\]%s\[\e[0;37m\])\[\e[0m\] ")\[\e[36m\]\w\[\e[0;33m\]\$\[\e[0m\] '
 parse_git_branch() {
      git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
 }
