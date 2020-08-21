903c3f9099741cfe7397ce54f8dbbca12339a77c
Bertrand Dubaut <bdubaut@gmail.com>
2020-08-21 08:32:22 +0000

    wip on aliases

--- shell/bash_aliases.sh
+++ shell/bash_aliases.sh
@@ -1,3 +1,17 @@
+# VIM and bash commands
+alias Bundle='vim +BundleInstall +qall'
+alias ll='ls -alF -G'
+alias la='ls -A -G'
+alias l='ls -CF -G'
+alias ..='cd ..'
+alias ...='cd ../../../'
+
+# Kubernetes Toolbox, to be modified manually on the machine for proper cluster
+# access.
+alias h="helm"
+alias k="kubectl"
+
+# GIT aliases
 alias gl='glg $(git show-ref | cut -d " " -f 2 | grep -v stash$) HEAD'
 alias glw='glp --word-diff'
 alias gsw='git show --format=fuller'
