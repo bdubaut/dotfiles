1b17509e5b52a23e68658ce1afec6402491a259f
Bertrand Dubaut <bdubaut@hey.com>
2020-08-31 16:07:25 +0200

    update $PATH

--- shell/path.sh
+++ shell/path.sh
@@ -1,4 +1,7 @@
 export PATH="/usr/local/Cellar/gnu-getopt/1.1.6/bin":$PATH
+export PATH="/usr/local/sbin:$PATH"
+export PATH="/usr/local/opt/openssl/bin:$PATH"
+export PATH="/usr/local/opt/ruby/bin:$PATH"
 export LDFLAGS="-L/usr/local/opt/readline/lib"
 export CPPFLAGS="-I/usr/local/opt/readline/include"
 
